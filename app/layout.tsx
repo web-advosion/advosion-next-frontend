e108aa0178bc7b6e0fddf85f82518ba9994978f5
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-13 11:20:14 +0100

    top section of footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} antialiased`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
