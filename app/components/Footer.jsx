88a6526eb743b3d665c7ea9c2bedafc77dcbc53d
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-16 14:59:48 +0100

    test commit after keychain reset

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -61,6 +61,7 @@ export default function Footer() {
                   ></Image>
                   <p>+45 40 82 71 01</p>
                 </Link>
+                test
               </li>
               <li>
                 <Link className="flex items-center gap-2" href="/">
