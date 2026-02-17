c9e28de933bae33aff2be63fbaf2127391f6a448
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-17 16:34:35 +0100

    updating pr for the card containers

--- app/components/AdvokatContainer.jsx
+++ app/components/AdvokatContainer.jsx
@@ -18,7 +18,7 @@ export default async function AdvokatContainer() {
     })),
   );
   return (
-    <div className="h-auto w-full flex overflow-x-auto gap-7.5 rounded-2xl py-2.5 pr-30 md:pr-40 lg:pr-100 xl:pr-200">
+    <div className="h-auto w-full flex overflow-x-auto gap-7.5 rounded-2xl py-2.5 pr-17 md:pr-40 lg:pr-100 xl:pr-200">
       {advokater.map((medarbejder) => (
         <AdvokatEmployeeCard key={medarbejder.id} medarbejder={medarbejder} />
       ))}
--- app/components/RevisorContainer.jsx
+++ app/components/RevisorContainer.jsx
@@ -12,7 +12,7 @@ export default async function RevisorContainer() {
   const revisorer = medarbejdere.filter((m) => m.acf.rolle === "revisor");
 
   return (
-    <div className="h-auto w-full flex overflow-x-auto gap-7.5 rounded-2xl py-2.5 pr-30 md:pr-40 lg:pr-100 xl:pr-200">
+    <div className="h-auto w-full flex overflow-x-auto gap-7.5 rounded-2xl py-2.5 pr-17 md:pr-40 lg:pr-100 xl:pr-200">
       {revisorer.map((medarbejder) => (
         <RevisorEmployeeCard key={medarbejder.id} medarbejder={medarbejder} />
       ))}
