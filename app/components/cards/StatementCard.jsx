58bf38ee2a0b875445981c66af13f619f0281a08
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-18 14:35:06 +0100

    Fixing statmentcard feature img problem

--- app/components/cards/StatementCard.jsx
+++ app/components/cards/StatementCard.jsx
@@ -1,27 +1,32 @@
 import Image from "next/image";
 
 export default function StatementCard({ statement }) {
+  // safely get featured image or fallback
+  const imageURL =
+    statement._embedded?.["wp:featuredmedia"]?.[0]?.source_url ||
+    "/img/image-error-message.svg";
+
   return (
-    <div className="w-64 h-125 bg-(--cards) rounded-2xl shadow-md items-center  justify-start flex flex-col gap-4 p-4">
+    <div className="w-64 h-125 bg-(--cards) rounded-2xl shadow-md items-center justify-start flex flex-col gap-4 p-4">
       <Image
         className="overflow-hidden rounded-2xl w-59 h-48 object-cover"
-        src={statement._embedded["wp:featuredmedia"][0].source_url}
-        alt={statement.title.rendered}
+        src={imageURL}
+        alt={statement.title?.rendered || "No title"}
         width={236}
         height={192}
       />
 
       <div className="w-56 h-60 justify-start">
         <h1 className="text-(--advokat-blue) text-lg font-bold font-['Inter'] leading-7">
-          {statement.acf.statement_title}
+          {statement.acf?.statement_title || "No title"}
         </h1>
 
         <div className="text-(--advokat-blue) italic">
-          <p className="italic">{statement.acf.statement_text}</p>
+          <p className="italic">{statement.acf?.statement_text || ""}</p>
         </div>
 
         <div className="mt-8">
-          <p>{statement.acf.client_name}</p>
+          <p>{statement.acf?.client_name || ""}</p>
         </div>
       </div>
     </div>
