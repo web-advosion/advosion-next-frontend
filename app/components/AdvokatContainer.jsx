459ed8ea67ef69094ef0159916adbfb440b1f459
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-20 16:02:29 +0100

    Changing query params to abc
    &orderby=title&order=asc
    
    for fetching alphabetically

--- app/components/AdvokatContainer.jsx
+++ app/components/AdvokatContainer.jsx
@@ -2,7 +2,7 @@ import AdvokatEmployeeCard from "./cards/AdvokatEmployeeCard";
 
 export default async function AdvokatContainer() {
   const res = await fetch(
-    "https://cms.advosion.dk/wp-json/wp/v2/medarbejder_kort?per_page=100&_embed",
+    "https://cms.advosion.dk/wp-json/wp/v2/medarbejder_kort?per_page=100&_embed&orderby=title&order=asc",
     { next: { revalidate: 10 } },
   );
 
--- app/components/RevisorContainer.jsx
+++ app/components/RevisorContainer.jsx
@@ -2,7 +2,7 @@ import RevisorEmployeeCard from "./cards/RevisorEmployeeCard";
 
 export default async function RevisorContainer() {
   const res = await fetch(
-    "https://cms.advosion.dk/wp-json/wp/v2/medarbejder_kort?per_page=100&_embed",
+    "https://cms.advosion.dk/wp-json/wp/v2/medarbejder_kort?per_page=100&_embed&orderby=title&order=asc",
     { next: { revalidate: 10 } },
   );
 
