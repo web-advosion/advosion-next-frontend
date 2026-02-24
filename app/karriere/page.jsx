a2a94e5b9fe4f6f775b9c7dfda7620b5b7f2e9ae
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-24 11:38:35 +0100

    Adding/Creating ButtonAbout

---
+++ app/karriere/page.jsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+import ButtonExperts from "../components/buttons/ButtonExperts";
+import SkillsContainerA from "../components/SkillsContainerA";
+import ExpertiseContainer from "../components/ExpertiseContainer";
+import ButtonContact from "../components/buttons/ButtonContact";
+import AdvokatEmployeeCard from "../components/cards/AdvokatEmployeeCard";
+import SingleEmployee from "../components/SingleEmployee";
+import SkillsContainerR from "../components/SkillsContainerR";
+import ButtonAbout from "../components/buttons/ButtonAbout";
+
+export default async function Page() {
+  const res = await fetch(
+    "https://cms.advosion.dk/wp-json/wp/v2/pages?slug=forside",
+    { next: { revalidate: 10 } }, // ISR: rebuild hver 10 sekunder
+  );
+  const pages = await res.json();
+  const page = pages[0];
+
+  return (
+    <main className="w-screen h-auto overflow-hidden flex flex-col items-center">
+      {/*Hero sektion */}
+
+      <section
+        id="hero"
+        className="w-screen h-auto px-7.5 md:px-10.75 lg:px-48 2xl:px-100 bg-(--primary-bg) mb-12.5 lg:mb-20 mt-12.5 relative "
+      >
+        <div className="md:flex">
+          <div>
+            <div className="justify-start text-CTA-black text-5xl font-bold font-['Inter'] leading-12 md:leading-9">
+              <h1>Bliv en del af holdet!</h1>
+            </div>
+
+            <div
+              data-aos="fade-in"
+              className="mt-5 lg:flex lg:gap-12.5 lg:items-start"
+            >
+              <div className=" w-full h-auto md:w-[90%]">
+                <p>
+                  Advosion var ikke noget uden{" "}
+                  <strong>dygtige medarbejdere</strong>! Måske du skal være den
+                  næste? <br />
+                  <br />
+                  Blive en del af en arbejdsplads, hvor fagligheden er i top og
+                  hvor der er plads og fleksibilitet til at få dit hverdagsskema
+                  til at gå op. <br /> <br />
+                  Er du nysgerrig på vores vision og værdier? Så kan du læse
+                  mere om hvem vi er under “Om os”.
+                </p>
+              </div>
+              <div className="mt-7.5">
+                <ButtonAbout />
+              </div>
+            </div>
+          </div>
+          <div className="w-full aspect-square md:max-w-96 md:max-h-96 lg:max-w-106 lg:max-h-106  md:min-w-96 md:min-h-96 lg:min-w-106 lg:min-h-106 relative overflow-hidden rounded-3xl shadow-md mt-12.5 md:mt-0 lg:mt-0">
+            <Image
+              className="object-cover"
+              src="/img/holdet-placeholder-hero.jpg"
+              fill
+              alt="Adovosion kollegaer står tæt sammen og kigger på sag"
+            ></Image>
+          </div>
+        </div>
+      </section>
+
+      {/*Sektion: Karriereudvikling */}
+      <section
+        id="karriere"
+        className="w-screen h-auto px-7.5 md:px-10.75 lg:px-48 2xl:px-100 bg-(--secondary-bg) pb-12.5 pt-12.5 lg:flex lg:justify-between lg:items-start"
+      >
+        <article className="lg:w-2/3">
+          <div className="justify-start text-CTA-black text-4xl font-bold font-['Inter'] flex items-end gap-3 leading-7">
+            <h2>Karriereudvikling</h2>
+          </div>
+
+          <div
+            className="mt-5 w-full lg:max-w-2/3 h-auto md:w-[80%]"
+            data-aos="fade-in"
+          >
+            <p>
+              Vi leverer{" "}
+              <strong>professionel økonomisk revision og rådgivning</strong>. Vi
+              har specialistviden og mangeårig erfaring særligt indenfor
+              følgende områder: <br />
+              <br /> Vores dygtige revisorer sikrer at du og din virksomhed
+              altid er i sikre hænder.
+            </p>
+          </div>
+          <div className="mt-7.5">
+            <ButtonContact />
+          </div>
+        </article>
+      </section>
+    </main>
+  );
+}
