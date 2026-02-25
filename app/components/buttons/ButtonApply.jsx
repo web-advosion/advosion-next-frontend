581939b2e28db67fbebf57f7dd4b10855981394d
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-02-25 11:57:05 +0100

    Fetching data from API for detailpage

--- app/components/buttons/ButtonApply.jsx
+++ app/components/buttons/ButtonApply.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-export default function ButtonContact({ bgColor }) {
+export default function ButtonApply() {
   return (
     <div className=" flex w-full">
       <Link
         className="max-w-40 h-8 bg-(--cta-black) px-8 rounded-2xl font-bold text-lg text-white flex items-center justify-center hover:scale-105 transition-transform duration-300 ease-in-out"
-        href="/kontakt"
+        href="/kontaktform"
       >
-        <p className="">Kontakt os</p>
+        <p className="">Ansøg</p>
       </Link>
     </div>
   );
--- app/karriere/[slug]/page.jsx
+++ app/karriere/[slug]/page.jsx
@@ -1,3 +1,6 @@
+import ButtonApply from "../../components/buttons/ButtonApply";
+import ButtonContact from "../../components/buttons/ButtonContact";
+
 export default async function DetailPage({ params }) {
   const { slug } = await params;
 
@@ -16,10 +19,40 @@ export default async function DetailPage({ params }) {
   return (
     <main className="w-screen h-auto overflow-hidden flex flex-col items-center bg-(--revision-blue)">
       <section
-        id="hero"
+        id="primary"
         className="w-screen h-auto px-7.5 md:px-10.75 lg:px-48 2xl:px-100 bg-(--primary-bg) mb-12.5 lg:mb-20 mt-12.5 relative"
       >
         <h1 className="text-4xl font-bold">{jobopslag.title.rendered}</h1>
+
+        <h2 className="text-xl font-normal font-['Inter'] leading-7">
+          {jobopslag.acf?.underoverskrift || "no subheader"}
+        </h2>
+
+        <p>{jobopslag.acf?.job_text || "no job description"}</p>
+
+        <h2>Arbejdsopgaver</h2>
+        <p>{jobopslag.acf?.arbejdsopgaver || "no job description"}</p>
+      </section>
+
+      {/*Sektion: Din profil */}
+      <section
+        id="profil"
+        className="w-screen h-auto px-7.5 mx-7.5 md:px-10.75 lg:px-48 2xl:px-100 mb-12.5 lg:mb-20 mt-12.5 relative"
+      >
+        <div className="w-80 h-160 p-4 origin-top-left bg-(--primary-bg) rounded-2xl">
+          <h1 className="text-4xl font-bold">Din profil</h1>
+
+          <p>{jobopslag.acf?.profilbeskrivelse || "no job description"}</p>
+
+          <article className="flex gap-2 py-7.5">
+            <div>
+              <ButtonContact />
+            </div>
+            <div>
+              <ButtonApply />
+            </div>
+          </article>
+        </div>
       </section>
     </main>
   );
