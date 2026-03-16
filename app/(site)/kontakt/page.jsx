12081915ec1c194f7fe6b770ed89e6f77851380b
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-03-16 15:01:18 +0100

    Fixing logo mt on LP

--- app/(site)/kontakt/page.jsx
+++ app/(site)/kontakt/page.jsx
@@ -11,58 +11,63 @@ export default async function Page() {
 
       <section
         id="hero"
-        className="w-screen h-auto px-7.5 md:px-10.75 lg:px-48 2xl:px-100 bg-(--primary-bg) mb-12.5 lg:mb-20 relative margin-to-nav margin-to-nav-mobile "
+        className="w-screen h-auto bg-(--primary-bg) mb-12.5 lg:mb-20 relative margin-to-nav margin-to-nav-mobile "
       >
-        <div className="md:flex justify-between">
-          <div>
-            <div className="justify-start text-CTA-black text-5xl font-bold font-['Inter'] leading-9">
-              <h1>Kontakt os</h1>
-            </div>
+        <div className="mx-auto w-full max-w-6xl px-6.5 lg:px-3.5">
+          <div className="md:flex justify-between">
+            <div>
+              <div className="justify-start text-CTA-black text-5xl font-bold font-['Inter'] leading-9">
+                <h1>Kontakt os</h1>
+              </div>
 
-            <div className="mt-5 lg:flex-col lg:gap-12.5 lg:items-start">
-              <div className=" w-full h-auto md:w-[90%]">
-                <p>
-                  Advosion var ikke noget uden{" "}
-                  <strong>dygtige medarbejdere!</strong> Måske du skal være den
-                  næste? <br /> <br />
-                  Blive en del af en arbejdsplads, hvor fagligheden er i top og
-                  hvor der er plads og fleksibilitet til at få dit hverdagsskema
-                  til at gå op. <br /> <br />
-                  Er du nysgerrig på vores vision og værdier? Så kan du læse
-                  mere om hvem vi er under “Om os”.
-                </p>
-              </div>
-              <div className="mt-7.5">
-                <ButtonFind />
+              <div className="mt-5 lg:flex-col lg:gap-12.5 lg:items-start">
+                <div className=" w-full h-auto md:w-[90%]">
+                  <p>
+                    Advosion var ikke noget uden{" "}
+                    <strong>dygtige medarbejdere!</strong> Måske du skal være
+                    den næste? <br /> <br />
+                    Blive en del af en arbejdsplads, hvor fagligheden er i top
+                    og hvor der er plads og fleksibilitet til at få dit
+                    hverdagsskema til at gå op. <br /> <br />
+                    Er du nysgerrig på vores vision og værdier? Så kan du læse
+                    mere om hvem vi er under “Om os”.
+                  </p>
+                </div>
+                <div className="mt-7.5">
+                  <ButtonFind />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full aspect-square md:max-w-96 md:max-h-96 lg:max-w-106 lg:max-h-106  md:min-w-96 md:min-h-96 lg:min-w-106 lg:min-h-106 relative overflow-hidden rounded-3xl shadow-md mt-12.5 md:mt-0 lg:mt-0">
-            <Image
-              className="object-cover"
-              src="/img/placeholder-kontakt-hero.svg"
-              fill
-              alt="Adovosion kollegaer står tæt sammen og kigger på sag"
-            ></Image>
+            <div className="w-full aspect-square md:max-w-96 md:max-h-96 lg:max-w-106 lg:max-h-106  md:min-w-96 md:min-h-96 lg:min-w-106 lg:min-h-106 relative overflow-hidden rounded-3xl shadow-md mt-12.5 md:mt-0 lg:mt-0">
+              <Image
+                className="object-cover"
+                src="/img/placeholder-kontakt-hero.svg"
+                fill
+                alt="Adovosion kollegaer står tæt sammen og kigger på sag"
+              ></Image>
+            </div>
           </div>
         </div>
       </section>
+
       {/*Sektion: kontakt */}
-      <section className=" w-full h-auto bg-(--primary-bg) px-7.5 md:px-10.75 lg:px-48 2xl:px-100">
-        <Link href="/">
-          <h1 className="font-bold text-3xl text-(--cta-black) mt-12.5 leading-7">
-            Kontakt
-          </h1>
-        </Link>
+      <section className=" w-full h-auto bg-(--primary-bg)">
+        <div className="mx-auto w-full max-w-6xl px-6.5 lg:px-3.5">
+          <Link href="/">
+            <h1 className="font-bold text-3xl text-(--cta-black) mt-12.5 leading-7">
+              Kontakt
+            </h1>
+          </Link>
 
-        {/*Card container */}
-        <div className="mt-7.5 w-full mb-25">
-          <div className="hidden md:block lg:block">
-            <ContactCard />
-          </div>
+          {/*Card container */}
+          <div className="mt-7.5 w-full mb-25">
+            <div className="hidden md:block lg:block">
+              <ContactCard />
+            </div>
 
-          <div className="md:hidden">
-            <ContactCardMobile />
+            <div className="md:hidden">
+              <ContactCardMobile />
+            </div>
           </div>
         </div>
       </section>
