91ab3023ddf720106cc486f4fc0018e4c6231cf2
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-03-26 15:26:54 +0100

    New revisor background hero

--- app/(site)/revision/page.jsx
+++ app/(site)/revision/page.jsx
@@ -48,7 +48,7 @@ export default async function Page() {
             <div className="w-full aspect-square md:max-w-96 md:max-h-96 lg:max-w-106 lg:max-h-106  md:min-w-96 md:min-h-96 lg:min-w-106 lg:min-h-106 relative overflow-hidden rounded-3xl shadow-md mt-12.5 md:mt-0 lg:mt-0">
               <Image
                 className="object-cover"
-                src="/img/accountant-hero-placeholder.jpg"
+                src="/img/DSC08310-1-5.jpg"
                 fill
                 alt="Adovosion kollegaer står tæt sammen og kigger på sag"
               ></Image>
