08dc3b9451302cc6a5a7c74d9fe34b7fcb444cdc
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-04-13 14:10:24 +0200

    Updating hero sec  text advokat

--- app/(site)/advokat/page.jsx
+++ app/(site)/advokat/page.jsx
@@ -32,16 +32,18 @@ export default async function Page() {
               <div className="mt-5 lg:flex lg:items-start">
                 <div className=" w-full h-auto md:w-[90%]">
                   <p>
-                    Vores partnere og advokater har en solid
-                    <strong> baggrund fra store danske advokatfirmaer</strong>.
-                    Dér har vi lært betydningen af kvalitet, faglig dygtighed og
-                    disciplin. <br /> <br />I ADVOSION har vi tilsat et
-                    <strong>kundeorienteret servicekoncept</strong>, hvor
-                    <strong>helhedsløsninger</strong> udvikles i tæt dialog med
-                    dig, så de skaber værdi for dig og din virksomhed. <br />{" "}
+                    Vores partnere og advokater har en solid baggrund fra nogle
+                    af Danmarks førende advokatfirmaer, hvor vi har opbygget en{" "}
+                    <strong>stærk faglighed</strong> og forståelse for kvalitet.
                     <br />
-                    Vi er passionerede omkring vores faglige kvalitet, finder de
-                    bedste løsninger, og når altid i mål.
+                    <br />I ADVOSION har vi bygget videre på den erfaring med en
+                    mere kundeorienteret tilgang, hvor vi udvikler løsninger i
+                    tæt dialog med dig – med fokus på, hvad der skaber{" "}
+                    <strong>reel værdi</strong> for din virksomhed.
+                    <br />
+                    <br />
+                    Vi er drevet af høj faglighed og arbejder målrettet for at
+                    finde de løsninger, der bedst understøtter dine behov.
                   </p>
                 </div>
               </div>
