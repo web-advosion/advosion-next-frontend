e40e0d930c9e674b8d5fb3610b9cea562d72b6d2
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-04-14 10:22:08 +0200

    Making contact MAP bigger
    -1/2 width
    -new responsive fitting
    -full height

--- app/components/cards/ContactCard.jsx
+++ app/components/cards/ContactCard.jsx
@@ -18,23 +18,24 @@ export default function ContactCard() {
           router.push("/kontakt");
         }
       }}
-      className="cursor-pointer w-full bg-(--cards) rounded-3xl shadow-md flex p-8 justify-between hover:scale-102 transition-transform duration-300 ease-in-out items-stretch gap-6"
+      className="cursor-pointer w-full max-h-96 bg-(--cards) rounded-3xl shadow-md flex p-0 justify-between hover:scale-102 transition-transform duration-300 ease-in-out items-stretch gap-6 overflow-hidden"
     >
-      <article className="flex-1 flex flex-col text-(--cta-black)">
-        <div className="space-y-2">
+      <article className="flex-1 flex flex-col text-(--cta-black) p-6 ">
+        <div className="space-y-4">
           <h2 className="text-2xl font-medium">
             <strong>Vi holder til i Skanderborg </strong> - et stenkast fra
             motorvejen
           </h2>
-          <p>
-            Kontakt os endelig og lad os finde ud af, hvordan vi kan hjælpe din
-            virksomhed
+          <p className="pb-2">
+            Du er altid velkommen til at kontakte os og høre, hvordan vi kan
+            hjælpe din virksomhed <br /> – find den rette medarbejder og tag
+            direkte fat i os.
           </p>
         </div>
 
-        <div className="mt-6">
-          <ul className="text-xl list-none">
-            <li className="flex items-center gap-2 my-5">
+        <div className="mt-0">
+          <ul className="text-xl list-none flex flex-wrap lg:mb-5">
+            <li className="flex items-center gap-2 my-0 mr-5">
               <Image
                 src="/img/icon-phone.svg"
                 width={22}
@@ -50,7 +51,7 @@ export default function ContactCard() {
               </a>
             </li>
 
-            <li className="flex items-center gap-2 my-5">
+            <li className="flex items-center gap-2 my-2">
               <Image
                 src="/img/icon-mail.svg"
                 width={22}
@@ -70,20 +71,22 @@ export default function ContactCard() {
           <Link
             href="/om/#holdet"
             onClick={(e) => e.stopPropagation()}
-            className="my-6 w-fit min-w-44 max-w-55 h-8 bg-(--cta-black) px-8 rounded-2xl font-bold text-lg text-white flex items-center justify-center hover:scale-105 transition-transform duration-300 ease-in-out"
+            className="mt-auto w-fit min-w-44 max-w-55 h-8 bg-(--cta-black) px-8 rounded-2xl font-bold text-lg text-white flex items-center justify-center hover:scale-105 transition-transform duration-300 ease-in-out"
           >
             Find medarbejder
           </Link>
         </div>
 
-        <div className="mt-auto text-5xl font-medium">Find os her</div>
+        <div className="mt-auto text-5xl invisible lg:visible font-medium">
+          Find os her
+        </div>
       </article>
 
       <article
-        className="w-1/2 xl:w-1/3 rounded-2xl overflow-hidden"
+        className="w-1/2"
         onClick={(e) => e.stopPropagation()} // prevents map click from navigating
       >
-        <div className="h-80 md:h-96 lg:h-96">
+        <div className="h-full">
           <GoogleMap />
         </div>
       </article>
