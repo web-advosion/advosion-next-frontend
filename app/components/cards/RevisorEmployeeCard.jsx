28d4c8c5d2a1c2de00fa5797ee8d98476c3a6b02
web-advosion <259890479+web-advosion@users.noreply.github.com>
2026-04-14 11:50:14 +0200

    Removing mt for employee cards
    To even out the padding

--- app/components/cards/RevisorEmployeeCard.jsx
+++ app/components/cards/RevisorEmployeeCard.jsx
@@ -17,7 +17,7 @@ export default function RevisorEmployeeCard({ medarbejder }) {
         />
       </div>
 
-      <div className="flex flex-col flex-1 mt-3 gap-5 text-(--primary-bg) p-4">
+      <div className="flex flex-col flex-1  gap-5 text-(--primary-bg) p-4">
         <div>
           <h1 className="text-xl font-bold">{medarbejder.acf.navn}</h1>
           <h2 className="text-base font-normal">{medarbejder.acf.stilling}</h2>
